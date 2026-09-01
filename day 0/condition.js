8341caa5e3715b58383a8ceade467ae4d931849b
Taha Dakhama <zorozezez189@gmail.com>
2026-09-01 14:53:44 +0200

    Add switch statement for age check
    Added a switch statement to check the age.

--- day 0/condition.js
+++ day 0/condition.js
@@ -1,9 +1,12 @@
+// Conditional Statements in JavaScript
+// if /else statement
 let age = 22;
 if (age >= 18) {
     console.log("You are an adult.");
 } else {
     console.log("You are not an adult.");
 }
+// switch statement
 switch (age) {
     case 22:
         console.log("You are 22 years old.");
