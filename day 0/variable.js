3860a419c26b275c64244ae439644ad1e17c7977
Wcode-yo <zorozezez189@gmail.com>
2026-09-01 14:37:10 +0200

    variable.js

---
+++ day 0/variable.js
@@ -0,0 +1,15 @@
+// int 
+let a=10; 
+console.log(a);
+// string
+let b="hello"; 
+console.log(b);
+//float
+let c=10.5; 
+console.log(c);
+// character
+let d='A';
+console.log(d);
+// boolean
+let e=true; 
+console.log(e);
