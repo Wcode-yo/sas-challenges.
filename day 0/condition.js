feb905ff0970dc28d1f5b157786dd764c0a3b2c4
Wcode-yo <zorozezez189@gmail.com>
2026-09-01 15:45:36 +0200

    +

--- day 0/condition.js
+++ day 0/condition.js
@@ -1,23 +1,15 @@
-// if /else 
+// Conditional Statements in JavaScript
+// if /else statement
 let age = 22;
 if (age >= 18) {
     console.log("You are an adult.");
 } else {
     console.log("You are not an adult.");
 }
-<<<<<<< HEAD
-=======
-// switch 
->>>>>>> aaea074b1a0b49461e20e57d6c1e0672c6cec67a
+// switch statement
 switch (age) {
-    case 18:
-    console.log("You are 18 years old.");
-    break;
     case 22:
-<<<<<<< HEAD
-    console.log("Yo
-=======
-    console.log("You are 22 years old.");
-    break;
+        console.log("You are 22 years old.");
+        break;
+
 }
->>>>>>> aaea074b1a0b49461e20e57d6c1e0672c6cec67a
