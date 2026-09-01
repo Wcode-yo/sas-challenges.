f8041c5f88bfea339dadaf2bfbd7fca3415f4ed9
Taha Dakhama <zorozezez189@gmail.com>
2026-09-01 12:57:53 +0200

    Add age condition checks using if/else and switch

--- day 0/condition.js
+++ day 0/condition.js
@@ -1,9 +1,11 @@
+// if /else 
 let age = 22;
 if (age >= 18) {
     console.log("You are an adult.");
 } else {
     console.log("You are not an adult.");
 }
+// switch 
 switch (age) {
     case 18:
     console.log("You are 18 years old.");
