1d27cf118e39ee75195fa0d9f3b22b64fc9f9438
Wcode-yo <zorozezez189@gmail.com>
2026-09-01 17:20:06 +0200

    +

---
+++ day 0/Level 2 (Intermediate)/Basic Calculator.js
@@ -0,0 +1,4 @@
+
+let num2 = 5 ;
+let num  = 10; 
+
---
+++ day 0/Level 2 (Intermediate)/grade.js
@@ -0,0 +1,10 @@
+let grade = 85;
+if (grade >= 90 && grade <= 100) {
+    console.log("A")
+} else if (grade >= 80&& grade <= 89) {
+    console.log("B")
+} else if (grade >= 70 && grade <= 79) {
+    console.log("C")
+} else if (grade >= 70) {
+    console.log("F")
+} 
