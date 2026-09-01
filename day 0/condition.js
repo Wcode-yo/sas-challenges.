29a50c3cb3efbba568a69295e485868d4890bca5
Taha Dakhama <zorozezez189@gmail.com>
2026-09-01 12:53:24 +0200

    Add switch statement for age cases

--- day 0/condition.js
+++ day 0/condition.js
@@ -1,7 +1,15 @@
 let age = 22;
-
+switch
 if (age >= 18) {
     console.log("You are an adult.");
 } else {
     console.log("You are not an adult.");
+}
+switch (age) {
+    case 18:
+    console.log("You are 18 years old.");
+    break;
+    case 22:
+    console.log("You are 22 years old.");
+    break;
 }
