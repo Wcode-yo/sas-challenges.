9464979f80800f7dd10d699460c4db7a45d4fce5
Wcode-yo <zorozezez189@gmail.com>
2026-09-02 14:23:38 +0100

    add

---
+++ day1/Level 1 (Beginner)/Multiplication Table.js
@@ -0,0 +1,5 @@
+let multip = 5 ; 
+for(let i = 1 ; i <= 10 ;i++){
+// multip = i*multip;
+console.log(multip * i);
+}
---
+++ day1/Level 1 (Beginner)/Sum of First 100 Numbers.js
@@ -0,0 +1,8 @@
+//sum first 100
+
+let sum = 0 ;
+for(let i=1;i<=100;i++)
+   {
+     sum += i ; 
+ console.log(sum);
+} 
