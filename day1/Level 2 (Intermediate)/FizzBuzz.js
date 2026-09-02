7ef3abe452a0ba5997ae9b2362d2912cebdc7b37
Wcode-yo <zorozezez189@gmail.com>
2026-09-02 15:13:18 +0100

    add

---
+++ day1/Level 2 (Intermediate)/FizzBuzz.js
@@ -0,0 +1,17 @@
+ 
+for (let b=1;b<=50;b++) {
+if (b%3===0){
+console.log("fizz");
+}else if(b%5===0){
+
+console.log("buzz");
+}
+else if(b%3===0&&b%5===0)
+{
+console.log("FizzBuzz");   
+}    
+else{
+console.log(b);   
+}
+
+}
