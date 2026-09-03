1b4512e3d7023b70e51f5d0f688d32c8fca7685c
Wcode-yo <zorozezez189@gmail.com>
2026-09-03 16:44:03 +0100

    add

---
+++ day2/chall algo/algo chall.js
@@ -0,0 +1,20 @@
+//chall 1 
+function afficherPaire(n){
+for(let i=0;i<=n;i++)
+if (i%2==0){
+console.log(i); 
+}
+  
+}afficherPaire(10);
+
+//chall 2 
+function compterPairs(nbr1, nbr2) {
+for(let i=nbr1;nbr1<=nbr2;i++)
+    if(nbr1&&nbr2%2==0){
+
+     
+    }
+
+}
+
+
