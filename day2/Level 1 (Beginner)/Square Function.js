a81e151b41df3217eef570a5921aa09f84b2974e
Wcode-yo <zorozezez189@gmail.com>
2026-09-03 13:44:38 +0100

    add

---
+++ day2/Level 1 (Beginner)/Square Function.js
@@ -0,0 +1,9 @@
+//use function square(num)
+function square(num){
+        let carre= num*num;
+        //use return statement 
+        return carre ; 
+    
+}
+// print result 
+console.log(square(4));
