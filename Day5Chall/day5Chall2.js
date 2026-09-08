908274b860e713780e55b78ce34ed7399f3fc3ba
Wcode-yo <zorozezez189@gmail.com>
2026-09-08 01:45:00 +0100

    add

---
+++ Day5Chall/day5Chall2.js
@@ -0,0 +1,13 @@
+let numbers = [12, 5, 31, 8, 19]; 
+let max = numbers[0] ; 
+for (let i=1;i<numbers.length;i++){
+if (numbers[i]>max){
+
+max=numbers[i]; 
+
+
+
+}
+
+
+}console.log("Largest: " +max);
